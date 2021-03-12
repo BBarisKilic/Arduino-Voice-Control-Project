5859016064106606dfc21db1274b24f35066f6b1
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-03-12 23:36:38 +0300

    Comments added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-String data;
-SoftwareSerial BTserial(2, 3);
 #define blink_led 10
+
+String data;
+//RX, TX
+SoftwareSerial BTserial(2, 3);
+
 
 void setup()
 {
+  //Starts software serial at 9600 Baudrate.
   BTserial.begin(9600); 
   Serial.begin(9600);
   pinMode(blink_led, OUTPUT);
@@ -14,6 +18,7 @@ void setup()
 
 void loop()
 {
+  //Resets the variable every time loop starts.
   data = "";
   
   while(BTserial.available()>0)
@@ -21,10 +26,13 @@ void loop()
     data +=BTserial.readString();
     Serial.println(data);
     if( data == "close" || data == "Close"){
+      //Turns off the led.
       digitalWrite(blink_led, LOW);   
     }else if( data == "open" || data == "Open"){
+      //Turns on the led.
       digitalWrite(blink_led, HIGH);  
     }else{
+      //The led blinks.
       digitalWrite(blink_led, HIGH); 
       delay(200);
       digitalWrite(blink_led, LOW); 
